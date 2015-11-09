16322d85ab2a76269d8bf472f3b84109f701ebd7
Alex <ajs476@nau.edu>
2015-11-08 21:01:16 -0700

    able to add items to list

--- app/src/main/java/cs399/sp/gatheryourgoods/CreateListActivity.java
+++ app/src/main/java/cs399/sp/gatheryourgoods/CreateListActivity.java
@@ -1,8 +1,13 @@
 package cs399.sp.gatheryourgoods;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,7 +16,11 @@ import java.util.ArrayList;
  * Created by Alex on 11/8/2015.
  */
 public class CreateListActivity extends AppCompatActivity {
-    public ArrayList<String> list;
+    public ArrayList<String> itemList;
+    public EditText addItemText;
+    public Button addItemButton;
+    public String item;
+    public ArrayAdapter<String> itemsAdapter;
 
 
     @Override
@@ -19,15 +28,32 @@ public class CreateListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
 
-        list = new ArrayList<String>();
-        list.add("Apples");
-        list.add("Bananas");
+        addItemText = (EditText)findViewById(R.id.editTextAddItem);
+        addItemButton = (Button)findViewById(R.id.buttonAddItem);
+
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                item = addItemText.getText().toString();
+                itemList.add(item);
+                itemsAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+        itemList = new ArrayList<String>();
+        // how to add to list:
+        itemList.add("Apples");
 
 
 
 
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+
+
+        itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, itemList);
 
         ListView listView = (ListView) findViewById(R.id.listViewItems);
         listView.setAdapter(itemsAdapter);
@@ -37,11 +63,14 @@ public class CreateListActivity extends AppCompatActivity {
     public void onStart(){
         super.onStart();
 
+
     }
+
 
     @Override
     public void onResume(){
         super.onResume();
+
 
     }
 
@@ -56,6 +85,7 @@ public class CreateListActivity extends AppCompatActivity {
         super.onDestroy();
 
     }
+
 
 }
 
